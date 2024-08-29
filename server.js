0feb8e2b55a039b66dab42abf08a5eaec17156ba
Luciano Filho <lcn.filho@gmail.com>
2024-08-29 19:41:35 -0300

    API GET

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const api = express()
+const porta = 8080
+
+api.get('/', (req, res) => {
+        const rotapadrao = 
+            {
+                nome_rota: '/',
+                codigo_status: '200',
+                metodo: 'GET'
+            }
+            res.status(200)
+            res.json(rotapadrao)
+})
+
+api.listen(porta, () =>{
+    console.log(`Servidor rodando na porta: ${porta}`)
+})
